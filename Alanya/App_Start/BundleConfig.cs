d4ebad8de9d7b0a3ecb6142d99dc092d503e8e13
alicoskun85 <alicoskun1@gmail.com>
2015-03-31 23:35:46 +0300

    fix

--- Alanya/App_Start/BundleConfig.cs
+++ Alanya/App_Start/BundleConfig.cs
@@ -9,29 +9,42 @@ namespace Alanya
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-         
+                      "~/Scripts/respond.js",
+                      "~/Scripts/infobubble.js",
+                      "~/Content/materialize/js/waves.js",
+                      "~/Scripts/CustomGoogleMapMarker.js",
+                      "~/Scripts/jquery.mmenu.min.all.js",
+                      "~/Content/lightSlider/js/jquery.lightSlider.js"
+                      ));
+
             //bundles.Add(new ScriptBundle("~/bundles/material").Include(
             //         "~/Scripts/materialize/js/materialize.min.js"));
             //bundles.Add(new ScriptBundle("~/Content/materialcss").Include(
             //        "~/Scripts/materialize/js/materialize.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-            //BundleTable.EnableOptimizations = true;
+                      "~/Content/site.css",
+                      "~/Content/panel.css",
+
+                      "~/Content/lightSlider/css/lightSlider.css",
+                      "~/Content/jquery.mmenu.all.css",
+                      "~/Content/rate/jquery.raty.css"
+                      ));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
