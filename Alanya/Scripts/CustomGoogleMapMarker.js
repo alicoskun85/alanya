6ec8aa120b3ec1ec4fff2b467e02fb060d0b47db
alicoskun85 <alicoskun1@gmail.com>
2015-04-02 16:38:14 +0300

    panelfix

--- Alanya/Scripts/CustomGoogleMapMarker.js
+++ Alanya/Scripts/CustomGoogleMapMarker.js
@@ -37,44 +37,44 @@ CustomMarker.prototype.draw = function () {
         google.maps.event.addDomListener(div, "click", function (event) {
 
             if ($('#panel').css('top') != "60px") {
-                $('#panel-body').toggle("slow");
+                $('#panel-body').toggle();
                 $('#panel').css({ top: '60px' });
-                $('#panel').css({ width: '145px' });
+                $('#panel').css({ width: '100px' });
             }
-            //service.getDetails(self.args.request, function (details, status) {
-            //    thumbsList = "";
-            //    PicsList = "";
-            //    var content = "<div class='scrollFix'><div class='detailTitle'>" + details.name + "</div>";
-            //    if (details.formatted_address != undefined) {
-            //        content += details.formatted_address + "<br />";
-            //    }
-            //    if (details.website != undefined) {
-            //        content += '<a href="' + details.website + '">' + details.website + '</a>' + "<br />";
-            //    }
-            //    if (details.rating != undefined) {
-            //        content += details.rating + "<br />";
-            //    }
-            //    if (details.formatted_phone_number != undefined) {
-            //        content += '<a href="tel:' + details.international_phone_number + '">' + details.formatted_phone_number + '</a>';
-            //    }
-            //    if (details.photos != undefined) {
-            //        modalTitle = details.name;
-            //        for (var i = 0; i < details.photos.length; i++) {
-            //            thumbsList += details.photos[i].getUrl({ 'maxWidth': 30, 'maxHeight': 30 }) + ",";
-            //            PicsList += details.photos[i].getUrl({ 'maxWidth': 300, 'maxHeight': 300 }) + ",";
-            //        }
-            //        content += "<a class='btn-floating btn pink lighten-3' style='float:right;margin-left: 10px;' onClick='imageDetayClick()'><i class='mdi-image-photo-camera'></i></a>";
-            //    }
-            //    if (details.reviews != undefined) {
-            //        modalTitle = details.name;
-            //        reviewsObj = details.reviews
-            //        content += "<a class='btn-floating btn pink lighten-3' style='float:right' onClick='reviewDetayClick()'><i class='mdi-editor-insert-comment'></i></a>";
-            //    }
-            //    content += "</div>";
+            service.getDetails(self.args.request, function (details, status) {
+                thumbsList = "";
+                PicsList = "";
+                var content = "<div class='scrollFix'><div class='detailTitle'>" + details.name + "</div>";
+                if (details.formatted_address != undefined) {
+                    content += details.formatted_address + "<br />";
+                }
+                if (details.website != undefined) {
+                    content += '<a href="' + details.website + '">' + details.website + '</a>' + "<br />";
+                }
+                if (details.rating != undefined) {
+                    content += details.rating + "<br />";
+                }
+                if (details.formatted_phone_number != undefined) {
+                    content += '<a href="tel:' + details.international_phone_number + '">' + details.formatted_phone_number + '</a>';
+                }
+                if (details.photos != undefined) {
+                    modalTitle = details.name;
+                    for (var i = 0; i < details.photos.length; i++) {
+                        thumbsList += details.photos[i].getUrl({ 'maxWidth': 30, 'maxHeight': 30 }) + ",";
+                        PicsList += details.photos[i].getUrl({ 'maxWidth': 300, 'maxHeight': 300 }) + ",";
+                    }
+                    content += "<a class='btn-floating btn pink lighten-3' style='float:right;margin-left: 10px;' onClick='imageDetayClick()'><i class='mdi-image-photo-camera'></i></a>";
+                }
+                if (details.reviews != undefined) {
+                    modalTitle = details.name;
+                    reviewsObj = details.reviews
+                    content += "<a class='btn-floating btn pink lighten-3' style='float:right' onClick='reviewDetayClick()'><i class='mdi-editor-insert-comment'></i></a>";
+                }
+                content += "</div>";
 
-            //    infoBubble.setContent(content);
-            //    infoBubble.open(map, self.args.marker);
-            //});
+                infoBubble.setContent(content);
+                infoBubble.open(map, self.args.marker);
+            });
         });
         var panes = this.getPanes();
         panes.overlayImage.appendChild(div);
