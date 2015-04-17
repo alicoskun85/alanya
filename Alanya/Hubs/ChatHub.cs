e05a6d12be17259f35f1c7feb826887edd0b7cf3
alicoskun85 <alicoskun1@gmail.com>
2015-04-17 11:38:13 +0300

    group send

--- Alanya/Hubs/ChatHub.cs
+++ Alanya/Hubs/ChatHub.cs
@@ -24,7 +24,7 @@ namespace Alanya.Hubs
             public string UserName { get; set; }
         }
 
-        public void Connect(string userName, string groupName)
+        public async System.Threading.Tasks.Task Connect(string userName, string groupName)
         {
             string conId = Context.ConnectionId;
 
@@ -41,19 +41,22 @@ namespace Alanya.Hubs
                 if (AllConnectedUsers.FirstOrDefault(a => a.Key == groupName).Value.Count(x => x.ConID == conId) == 0)
                 {
                     AllConnectedUsers.FirstOrDefault(a => a.Key == groupName).Value.Add(new Users { ConID = conId, UserName = userName, GroupName = groupName });
-
                 }
-
             }
             Clients.Caller.onConnected(userName, conId, groupName);
+
+            await Groups.Add(Context.ConnectionId, groupName);
             Clients.Group(groupName).showAllUsers(AllConnectedUsers.FirstOrDefault(a => a.Key == groupName).Value);
-            //    Clients.Group(groupName).hello();
+            Clients.Group(groupName).sendMessage(userName + " joined.", "system");
+
+            //Clients.Group(groupName).hello();
+
         }
 
         public void SendMessage(MyMessage Message)
         {
             //Clients.All.sendMessage(Message.Msg, Message.UserName);
-            Clients.Group(Message.GroupName).sendMessage("Group Message " + Message.Msg);
+            Clients.Group(Message.GroupName).sendMessage(Message.Msg, Message.UserName);
         }
 
         public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
@@ -70,6 +73,7 @@ namespace Alanya.Hubs
                 //AllConnectedUsers..Remove(user);
                 var conId = Context.ConnectionId;
                 Clients.All.onUserDisconnected(conId);
+                Groups.Remove(Context.ConnectionId, cUser.GroupName);
             }
             return base.OnDisconnected(stopCalled);
         }
